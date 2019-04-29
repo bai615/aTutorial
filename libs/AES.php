7f978cf8d65d1cb795c1157e723da52046e60cc6
baihua <baihua_2011@163.com>
2019-04-29 13:24:49 +0800

    加密 (适用 PHP 5)

--- libs/AES.php
+++ libs/AES.php
@@ -41,4 +41,31 @@ class AES
         $m = openssl_decrypt(base64_decode($encrypted), 'aes-128-cbc', $key, OPENSSL_NO_PADDING, $iv);
         return (rtrim(rtrim($m, chr(0)), chr(7)));
     }
+
+    /**
+     * 加密 (适用 PHP 5)
+     * @param $unencrypted
+     * @param $key
+     * @param $iv
+     * @return string
+     */
+    public function encrypt5($unencrypted, $key128, $iv)
+    {
+        $cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+        
+        if (mcrypt_generic_init($cipher, $key128, $iv) != -1) {
+            
+            if (is_array($unencrypted)) {
+                $str_padded = json_encode($unencrypted);
+            } else {
+                $str_padded = $unencrypted;
+            }
+            $cipherText = mcrypt_generic($cipher, $str_padded);
+            mcrypt_generic_deinit($cipher);
+
+            return base64_encode($cipherText);
+        }
+
+        return '';
+    }
 }
