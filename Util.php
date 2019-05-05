b4d2584f459967b02cfbf48f2459852bf2a37f10
baihua <baihua_2011@163.com>
2019-05-05 21:08:14 +0800

    驼峰命名转下划线命名

--- Util.php
+++ Util.php
@@ -15,3 +15,12 @@ function getMicroTime()
     list($msec, $sec) = explode(' ', microtime());
     return (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
 }
+
+//驼峰命名转下划线命名
+function toUnderScore($str)
+{
+    $dstr = preg_replace_callback('/([A-Z]+)/', function ($matchs) {
+        return '_' . strtolower($matchs[0]);
+    }, $str);
+    return trim(preg_replace('/_{2,}/', '_', $dstr), '_');
+}
